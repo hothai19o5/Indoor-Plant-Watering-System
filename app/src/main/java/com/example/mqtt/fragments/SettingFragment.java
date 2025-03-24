101a656afc7bc747a53c75c6eed5fd1f017ea38a
Ho Thai <thai.hx04@gmail.com>
2025-03-24 18:52:06 +0700

    RESET esp8266 & Logout -!! Bug: Login with Google

--- app/src/main/java/com/example/mqtt/fragments/SettingFragment.java
+++ app/src/main/java/com/example/mqtt/fragments/SettingFragment.java
@@ -4,10 +4,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.example.mqtt.MainActivity;
 import com.example.mqtt.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SettingFragment extends Fragment {
     
@@ -18,8 +23,27 @@ public class SettingFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         view = inflater.inflate(R.layout.fragment_setting_page, container, false);
+
+        Button resetEspButton = view.findViewById(R.id.resetButton);
+        Button logoutButton = view.findViewById(R.id.logoutButton);
+
+        resetEspButton.setOnClickListener(v -> {
+            if (getActivity() instanceof MainActivity) {
+                ((MainActivity) getActivity()).sendCommand("RESET");
+            }
+        });
+
+        logoutButton.setOnClickListener(v -> {
+            // Logout from Firebase
+            FirebaseAuth.getInstance().signOut();
+
+            // Return to login screen
+            if (getActivity() instanceof MainActivity) {
+                ((MainActivity) getActivity()).navigateToLogin();
+            }
+        });
         
         return view;
     }
-   
+
 }
